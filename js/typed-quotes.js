714bf3624aa35d3df27776ae3d458413d618cefe
PengZong <pengzong771@163.com>
2016-11-22 06:11:53 +0800

    更新

---
+++ js/typed-quotes.js
@@ -0,0 +1,23 @@
+/**
+ * Created by PengZong on 16/11/22.
+ */
+var s = 'Happy birthday!';
+var con = $('.typed-quotes');
+var index = 0;
+var length = s.length;
+var tId = null;
+
+function start(){
+    con.text('');
+
+    tId=setInterval(function(){
+        con.append(s.charAt(index));
+        if(index++ === length){
+            clearInterval(tId);
+            index = 0;
+            start()
+        }
+    },100);
+}
+
+start();
